601f68819569236ff47ad00137214b04bf90d1c0
Forkentiney <jseverud@proton.me>
2026-02-19 15:39:53 -0600

    Added add/cancel visual swapping

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const joshuasBooks = [];
 
+const newBookForm = document.querySelector("form");
+const newBook = document.querySelector("#newBook");
+const newButton = document.querySelector("#newButton");
+const uploadButton = document.querySelector("#submitNewBook");
+const cancel = document.querySelector("#cancel");
+
+let title = newBookForm.elements.title;
+let author = newBookForm.elements.author;
+let year = newBookForm.elements.year;
+
+cancel.addEventListener("click", hideForm);
+
+newBook.addEventListener("click", showForm);
+
+function hideForm() {
+  newButton.classList.remove("hidden");
+  newBookForm.classList.add("hidden");
+};
+
+function showForm() {
+  newButton.classList.add("hidden");
+  newBookForm.classList.remove("hidden");
+};
+
 function Book(title, author, year, status) {
   if (!new.target) {
     throw Error("You must use the 'new' operator");
@@ -8,8 +32,7 @@ function Book(title, author, year, status) {
   this.author = author;
   this.year = year;
   this.status = status;
-}
+};
 
 function addBook() {
-
-}
+};
