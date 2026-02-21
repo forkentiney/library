872fb67e40f9846640448f3e6261f49f899537fc
Forkentiney <jseverud@proton.me>
2026-02-21 13:55:59 -0600

    Added books to array
    Books removed from html file and added to js file. Books are now added
    to div from the array which is where new books put when added.

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ const newBookForm = document.querySelector("form");
 const newButton = document.querySelector("#newButton");
 const uploadButton = document.querySelector("#submitNewBook");
 const cancel = document.querySelector("#cancel");
+const shelf = document.querySelector("#shelf");
 
 const totalityAndInfinity = new Book("Totality and Infinity", "Emmanual Levinas", 1961, "read", "1");
+const beingAndTime = new Book("Being and Time", "Martin Heidegger", 1927, "read", "2");
+const nineteenEightyFour = new Book("1984", "George Orwell", 1949, "unread", "3");
+const allQuietOnTheWesternFront = new Book("All Quiet on the Western Front", "Erich Maria Remarque", 1928, "unread", "4");
+const theHobbit = new Book("The Hobbit", "J. R. R. Tolkien", 1937, "read", "5");
 
-let joshuasBooks = [totalityAndInfinity];
+let joshuasBooks = [totalityAndInfinity, beingAndTime, nineteenEightyFour, allQuietOnTheWesternFront, theHobbit];
 
 cancel.addEventListener("click", hideForm);
 newButton.addEventListener("click", showForm);
@@ -31,6 +36,22 @@ function Book(title, author, year, status, id) {
   this.id = id;
 };
 
+function putBooksOnShelf() {
+  for (let i = 0; i < joshuasBooks.length; i++) {
+    const book = document.createElement("div");
+    const title = document.createElement("h2");
+    const author = document.createElement("h3");
+    book.setAttribute("class", "book");
+    title.textContent = `${joshuasBooks[i].title}`;
+    author.textContent = `${joshuasBooks[i].author}`;
+
+    shelf.insertBefore(book, newButton);
+
+    book.appendChild(title);
+    book.appendChild(author);
+  }
+}
+
 function addBook(event) {
   let title = newBookForm.elements.title.value;
   let author = newBookForm.elements.author.value;
@@ -43,3 +64,5 @@ function addBook(event) {
   event.preventDefault();
   hideForm();
 };
+
+putBooksOnShelf();
